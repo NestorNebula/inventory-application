b1931dae0ab38d9d9adacd984de96cd424b684e5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-23 13:29:41 +0200

    feat: add SQL code to populate the db

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,6 +1,31 @@
 const { Client } = require('pg');
 require('dotenv').config();
 
+const SQL = `
+CREATE TABLE IF NOT EXISTS authors (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(25)
+);
+
+CREATE TABLE IF NOT EXISTS genres (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  genre VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS books (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(50),
+  pages VARCHAR(4),
+  plot TEXT,
+  author_id INTEGER REFERENCES authors(id)
+);
+
+CREATE TABLE IF NOT EXISTS books_genres (
+  book_id INTEGER REFERENCES books(id),
+  genre_id INTEGER REFERENCES genres(id)
+);
+`;
+
 async function main() {
   const client = new Client({
     connectionString: process.env.LOCAL_DB,
