84102eff5c88975bfeb6c735b26d08c96b081dd4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-23 16:44:12 +0200

    feat: import authorsController in authorsRouter

--- routes/authorsRouter.js
+++ routes/authorsRouter.js
@@ -1,4 +1,10 @@
 const { Router } = require('express');
 const authorsRouter = Router();
+const authorsControllers = require('../controllers/authorsController');
+
+authorsRouter.get('/:author', authorsControllers.getAuthor);
+authorsRouter.post('/:author', authorsControllers.updateAuthorPost);
+authorsRouter.post('/:author/delete', authorsControllers.deleteAuthorPost);
+authorsRouter.post('/', authorsControllers.createAuthorPost);
 
 module.exports = authorsRouter;
