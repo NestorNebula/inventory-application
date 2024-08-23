eac9e89798e84a98f3b726a86453857103ccc6f3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-23 15:43:39 +0200

    feat: add ejs in app config

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('node:path');
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT;
 app.listen(PORT);
