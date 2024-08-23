af03a59f5a629975cb3743592ed7b727d7dc5cde
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-23 16:14:13 +0200

    feat: add middlewares for different routes

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require('express');
 require('dotenv').config();
 const path = require('node:path');
 const app = express();
+const indexRouter = require('./routes/indexRouter');
+const genresRouter = require('./routes/genresRouter');
+const authorsRouter = require('./routes/authorsRouter');
+const booksRouter = require('./routes/booksRouter');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+app.use('/genre', genresRouter);
+app.use('/author', authorsRouter);
+app.use('/book', booksRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT);
