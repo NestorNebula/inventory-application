57fcf6cba38265a6525cf92ef7e2bdda72b2762f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-23 18:00:09 +0200

    feat: create queries file
    The file contains comments on the application's needed functions

---
+++ db/queries.js
@@ -0,0 +1,32 @@
+const pool = require('./pool');
+
+// GENERAL QUERIES
+// getAllGenres
+// getAllAuthors
+
+// GENRES QUERIES
+// getGenre(id)
+// getBooksByGenre(id)
+// updateGenre(genre)
+// deleteGenre(id)
+// insertGenre(genre)
+
+// AUTHORS QUERIES
+// getAuthor(id)
+// getBooksByAuthor(id)
+// updateAuthor(author)
+// deleteAuthor(id)
+// insertAuthor(author)
+
+// BOOKS QUERIES
+// getBook(id)
+// getBookInformation(id)
+// updateBook(book)
+// deleteBook(id)
+// insertBook(book)
+// removeAuthorOffBooks(authorId)
+
+// BOOKS_GENRES DB QUERIES
+// deleteBookOffGenres(bookId)
+// deleteGenreOffBooks(genreId)
+// insertBookGenre(bookId, genreId)
