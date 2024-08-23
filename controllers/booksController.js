3c4f344c4d59fd160781a9c664b121d4120bde55
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-23 16:49:13 +0200

    feat: create books controller

---
+++ controllers/booksController.js
@@ -0,0 +1,9 @@
+function getBook() {}
+
+function updateBookPost() {}
+
+function deleteBookPost() {}
+
+function createBookPost() {}
+
+module.exports = { getBook, updateBookPost, deleteBookPost, createBookPost };
