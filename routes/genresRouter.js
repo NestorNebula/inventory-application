bb4c0800af9d5b6eadc13b7762f5fa1312d6ce36
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-23 16:38:38 +0200

    feat: import genresController in genresRouter

--- routes/genresRouter.js
+++ routes/genresRouter.js
@@ -1,4 +1,10 @@
 const { Router } = require('express');
 const genresRouter = Router();
+const genresController = require('../controllers/genresController');
+
+genresRouter.get('/:genre', genresController.getGenre);
+genresRouter.post('/:genre', genresController.updateGenrePost);
+genresRouter.post('/:genre/delete', genresController.deleteGenrePost);
+genresRouter.post('/', genresController.createGenrePost);
 
 module.exports = genresRouter;
