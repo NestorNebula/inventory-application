c8db9964eaca95f1f95d8798f45d9a8f328ffc0f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-23 16:50:36 +0200

    feat: import booksController in booksRouter

--- routes/booksRouter.js
+++ routes/booksRouter.js
@@ -1,4 +1,10 @@
 const { Router } = require('express');
 const booksRouter = Router();
+const booksController = require('../controllers/booksController');
+
+booksRouter.get('/:book', booksController.getBook);
+booksRouter.post('/:book', booksController.updateBookPost);
+booksRouter.post('/:book/delete', booksController.deleteBookPost);
+booksRouter.post('/', booksController.createBookPost);
 
 module.exports = booksRouter;
