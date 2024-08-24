0bf02cc74ffaec51be0ec840d59139e7ae7fdc6b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-24 17:52:35 +0200

    feat: add error handling middleware in app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const indexRouter = require('./routes/indexRouter');
 const genresRouter = require('./routes/genresRouter');
 const authorsRouter = require('./routes/authorsRouter');
 const booksRouter = require('./routes/booksRouter');
+const customError = require('./modules/error');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,6 +16,9 @@ app.use('/', indexRouter);
 app.use('/genre', genresRouter);
 app.use('/author', authorsRouter);
 app.use('/book', booksRouter);
+app.use((req, res, next) => {
+  throw new customError("This page doesn't exist.", 404, 'Page not found');
+});
 
 app.use((err, req, res, next) => {
   console.error(err);
