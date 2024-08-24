6aabbb02d355645d656dcb593f1775c1a43f5994
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-24 17:12:53 +0200

    feat: add path to public files in app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const booksRouter = require('./routes/booksRouter');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/genre', genresRouter);
