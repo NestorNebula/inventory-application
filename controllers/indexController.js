ba91da226ff011c04f61b3a80bc8f352243b40cd
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-24 12:08:31 +0200

    feat: add getIndexPage function

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,15 @@
-function getIndexPage() {}
+const db = require('../db/queries');
+
+function getIndexPage(req, res) {
+  const genres = db.getAllGenres();
+  const authors = db.getAllAuthors();
+  Promise.all([genres, authors]).then((values) => {
+    res.render('index', {
+      title: 'Inventory Homepage',
+      genres: values[0],
+      authors: values[1],
+    });
+  });
+}
 
 module.exports = { getIndexPage };
