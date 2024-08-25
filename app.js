2eee9fbaf87fd5d51d0c2d55b72a0392a3125e26
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-25 17:45:50 +0200

    feat: use urlencoded for forms

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const customError = require('./modules/error');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 app.use('/genre', genresRouter);
