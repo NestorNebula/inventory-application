b949e3adbdfa289b13b55151cbbf1e034d15ae83
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-25 10:40:20 +0200

    feat: add getAuthor function

--- controllers/authorsController.js
+++ controllers/authorsController.js
@@ -1,4 +1,22 @@
-function getAuthor() {}
+const db = require('../db/queries');
+const customError = require('../modules/error');
+
+function getAuthor(req, res) {
+  const author = db.getAuthor(req.params.author);
+  const books = db.getBooksByAuthor(req.params.author);
+  Promise.all([author, books])
+    .then((values) => {
+      res.render('author', { author: values[0][0], books: values[1] });
+    })
+    .catch((error) => {
+      console.error(error);
+      throw new customError(
+        "The server couldn't load the data you required.",
+        500,
+        'Server Error'
+      );
+    });
+}
 
 function updateAuthorPost() {}
 
