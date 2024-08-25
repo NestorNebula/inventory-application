486bc2e349367551d39e940f550c0d3e283ea2a1
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-25 10:18:31 +0200

    feat: add getGenre function to genresController

--- controllers/genresController.js
+++ controllers/genresController.js
@@ -1,4 +1,22 @@
-function getGenre() {}
+const db = require('../db/queries');
+const customError = require('../modules/error');
+
+function getGenre(req, res) {
+  const genre = db.getGenre(req.params.genre);
+  const books = db.getBooksByGenre(req.params.genre);
+  Promise.all([genre, books])
+    .then((values) => {
+      res.render('genre', { genre: values[0][0], books: values[1] });
+    })
+    .catch((error) => {
+      console.error(error);
+      throw new customError(
+        "The server couldn't load the data you required.",
+        500,
+        'Server Error'
+      );
+    });
+}
 
 function updateGenrePost() {}
 
