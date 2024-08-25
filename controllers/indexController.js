e05c3fa18b55182c9faa60b0d875be83400592d7
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-25 09:30:41 +0200

    feat: add error handler in indexController

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,15 +1,25 @@
 const db = require('../db/queries');
+const customError = require('../modules/error');
 
 function getIndexPage(req, res) {
   const genres = db.getAllGenres();
   const authors = db.getAllAuthors();
-  Promise.all([genres, authors]).then((values) => {
-    res.render('index', {
-      title: 'Inventory Homepage',
-      genres: values[0],
-      authors: values[1],
+  Promise.all([genres, authors])
+    .then((values) => {
+      res.render('index', {
+        title: 'Inventory Homepage',
+        genres: values[0],
+        authors: values[1],
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      throw new customError(
+        "The server couldn't load the data.",
+        500,
+        'Server Error'
+      );
     });
-  });
 }
 
 module.exports = { getIndexPage };
