dbd187c355a778379d6281dae172d46ef16f9afe
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-25 11:41:19 +0200

    feat: add getBook function

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,4 +1,19 @@
-function getBook() {}
+const db = require('../db/queries');
+const customError = require('../modules/error');
+
+async function getBook(req, res, next) {
+  const results = await db.getBookInformations(req.params.book);
+  if (!results.length) {
+    next(
+      new customError(
+        "The book you searched doesn't exist.",
+        404,
+        'Book Not Found'
+      )
+    );
+  }
+  res.render('book', { book: results[0], genres: results });
+}
 
 function updateBookPost() {}
 
