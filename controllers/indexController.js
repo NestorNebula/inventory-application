9539fac29ff46861d53d890f9102085afeac89fa
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-28 17:48:36 +0200

    feat: add letters value passed to the index views
    Letters will be an array of all first letters in the authors results

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,10 +6,15 @@ function getIndexPage(req, res, next) {
   const authors = db.getAllAuthors();
   Promise.all([genres, authors])
     .then((values) => {
+      let letters = values[1].map((author) => author.name[0].toLowerCase());
+      letters = letters
+        .sort()
+        .filter((letter, index) => letters.indexOf(letter) === index);
       res.render('index', {
         title: 'Inventory Homepage',
         genres: values[0],
         authors: values[1],
+        letters: letters,
       });
     })
     .catch((error) => {
