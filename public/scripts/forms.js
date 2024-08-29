20cc5a2c49a516b3abcb02cc101bf4f9fbba70be
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-29 16:57:22 +0200

    style: rearrange styles and add some to genre.ejs

--- public/scripts/forms.js
+++ public/scripts/forms.js
@@ -1,6 +1,6 @@
-const addButtons = document.querySelectorAll('.plus-button');
-const forms = document.querySelectorAll('.home-form');
-addButtons.forEach((button, index) => {
+const formOpener = document.querySelectorAll('.opener');
+const forms = document.querySelectorAll('.dynamic');
+formOpener.forEach((button, index) => {
   button.addEventListener('click', () => {
     forms[index].classList.contains('hidden')
       ? forms[index].classList.remove('hidden')
