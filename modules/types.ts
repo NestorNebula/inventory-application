c78644416fd55b01eafff096cb4b4067aac02530
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-08 18:31:12 +0100

    feat: create Author, Genre & Book interfaces

--- modules/types.ts
+++ modules/types.ts
@@ -1,1 +1,22 @@
+interface Author {
+  id: number;
+  name: string;
+}
 
+interface Genre {
+  id: number;
+  genre: string;
+  image?: string;
+  book_id?: number;
+}
+
+interface Book {
+  id: number;
+  title: string;
+  pages: number;
+  plot?: string;
+  author_id?: number;
+  genre_id?: number;
+}
+
+export type { Author, Genre, Book };
