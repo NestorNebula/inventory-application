773d6a9325505f5159d72dc65bb3ee58b444c436
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-09 07:42:06 +0100

    refactor: change language to TypeScript

--- routes/booksRouter.ts
+++ routes/booksRouter.ts
@@ -1,10 +1,10 @@
-const { Router } = require('express');
+import { Router } from 'express';
 const booksRouter = Router();
-const booksController = require('../controllers/booksController');
+import booksController from '../controllers/booksController';
 
 booksRouter.get('/:book', booksController.getBook);
 booksRouter.post('/:book', booksController.updateBookPost);
 booksRouter.post('/:book/delete', booksController.deleteBookPost);
 booksRouter.post('/', booksController.createBookPost);
 
-module.exports = booksRouter;
+export default booksRouter;
