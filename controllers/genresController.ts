d37e4e7d2375e0d1c4d96eec8836f0bd79dcd634
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-09 09:00:49 +0100

    refactor: change language to TypeScript

---
+++ controllers/genresController.ts
@@ -0,0 +1,106 @@
+import * as db from '../db/queries';
+import customError from '../modules/error';
+import { validationResult } from 'express-validator';
+import { getErrorMessage } from '../modules/validation';
+import { NextFunction, Request, Response } from 'express';
+require('dotenv').config();
+
+function getGenre(req: Request, res: Response, next: NextFunction) {
+  const genre = db.getGenre(+req.params.genre);
+  const books = db.getBooksByGenre(+req.params.genre);
+  Promise.all([genre, books])
+    .then((values) => {
+      res.render('genre', { genre: values[0][0], books: values[1] });
+    })
+    .catch((error) => {
+      console.error(error);
+      next(
+        new customError(
+          "The server couldn't load the data you required.",
+          500,
+          'Server Error'
+        )
+      );
+    });
+}
+
+const updateGenrePost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const message = getErrorMessage(errors);
+    next(
+      new customError(
+        `The form wasn't submitted correctly. ${message}`,
+        400,
+        'Incorrect Form'
+      )
+    );
+    return;
+  }
+  const genre = {
+    genre: req.body.updated_genre,
+    id: +req.params.genre,
+  };
+  await db.updateGenre(genre);
+  res.redirect(`/genre/${genre.id}`);
+};
+
+async function deleteGenrePost(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.body.password !== process.env.PASSWD) {
+    next(
+      new customError(
+        "Can't delete the genre, the password is incorrect.",
+        400,
+        'Incorrect Password'
+      )
+    );
+    return;
+  }
+  await db.deleteGenreFromBooks(+req.params.genre);
+  await db.deleteGenre(+req.params.genre);
+  res.redirect('/');
+}
+
+const createGenrePost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const message = getErrorMessage(errors);
+    next(
+      new customError(
+        `The form wasn't submitted correctly. ${message}`,
+        400,
+        'Incorrect Form'
+      )
+    );
+    return;
+  }
+  const { newgenre } = req.body;
+  const same = await db.getGenreByName(newgenre);
+  if (same.length > 0) {
+    next(
+      new customError('This genre already exists.', 400, 'Genre already exists')
+    );
+    return;
+  }
+  await db.insertGenre(newgenre);
+  res.redirect('/');
+};
+
+export default {
+  getGenre,
+  updateGenrePost,
+  deleteGenrePost,
+  createGenrePost,
+};
